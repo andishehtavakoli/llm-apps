216ff91680be1989074b73669791238a1ae59624
andishehtavakoli <atava003@ucr.edu>
2024-06-11 11:58:20 -0700

    multi_agents_llm

---
+++ src/get_model.sh
@@ -0,0 +1,10 @@
+#!/bin/zsh
+# variables
+
+# For pulling llama2 
+model_name="llama2"
+custom_model_name="crewai-llama2"
+#get the base model
+ollama pull $model_name
+#create the model file
+ollama create $custom_model_name -f ./Llama2ModelFile
